7c9c823a6f340915cd1eae16adc4da0bab42fbe0
Yaron-Nagauker <76478484+Yaron-Nagauker@users.noreply.github.com>
2022-03-22 11:44:02 +0200

    update-1

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
-const cover = document.getElementById('cover')
+const cover = document.getElementById('cover-1')
 const signin = document.getElementById('sign-in')
 const login = document.getElementById('log-in')
 const btnSubmitLogin = document.getElementById('btn-login')
 const btnSubmitSignup = document.getElementById('btn-signup')
+const loginForm = document.getElementById('login-form')
+const signupForm = document.getElementById('signup-form')
 
 const loginEmail = document.getElementById('login-email')
 const loginPass = document.getElementById('login-pass')
@@ -14,16 +16,19 @@ const signUpEmail = document.getElementById('')
 const signUpPass = document.getElementById('')
 
 
-
-
 signin.addEventListener('click',(event)=>{
-    console.log(event)
+    // console.log(event)
     cover.classList.remove('open')
+    loginForm.classList.add('close')
+    signupForm.classList.remove('close')
 })
 
 login.addEventListener('click',(event)=>{
-    console.log(event)
+    // console.log(event)
     cover.classList.add('open')
+    loginForm.classList.remove('close')
+    signupForm.classList.add('close')
+    // Disabled inputs
     
 })
 
